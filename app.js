dcde8d8ac4e9bb6b4c6c55b6e0eed1efea004cc9
suyash2k04 <128306540+suyash2k04@users.noreply.github.com>
2023-12-19 22:31:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const navbar = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const closeBtn = document.querySelector("#close-btn");
+const sidebar = document.querySelector("#sidebar");
+const date = document.querySelector("#date");
+
+// add fixed class to navbar
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 80) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
+});
+// show sidebar
+navBtn.addEventListener("click", function () {
+  sidebar.classList.add("show-sidebar");
+});
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
+// set year
+date.innerHTML = new Date().getFullYear();
+
+function myfun(){
+  var element = document.body;
+  element.dataset.bstheme = 
+  element.dataset.bstheme == "light" ? "dark" : "light";
+}
